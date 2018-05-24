a70f0ad40c5a79e99953599d170ee3db9051dd52
John Fulton <john@TechElevator.com>
2018-05-23 20:12:18 -0400

    m1-w1d4-loops-arrays-exercises

---
+++ m1-w1d4-loops-arrays-exercises/Exercises/Sum13.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public partial class Exercises
+    {
+        /*
+         Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 
+         13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do 
+         not count.
+         sum13([1, 2, 2, 1]) → 6
+         sum13([1, 1]) → 2
+         sum13([1, 2, 2, 1, 13]) → 6
+         */
+        public int Sum13(int[] nums)
+        {
+            return 0;
+        }
+
+    }
+}

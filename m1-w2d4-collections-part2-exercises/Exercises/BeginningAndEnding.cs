b2459e87c9d9ce8fd83296fbf8b521a680db6870
John Fulton <john@TechElevator.com>
2018-05-31 05:51:12 -0400

    m1-w2d4-collections-part2-exercises

---
+++ m1-w2d4-collections-part2-exercises/Exercises/BeginningAndEnding.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public partial class Exercises
+    {
+        /*
+         * Given an array of non-empty strings, return a Dictionary<string, string> where for every different string in the array, 
+         * there is a key of its first character with the value of its last character.
+         *
+         * beginningAndEnding(["code", "bug"]) → {"b": "g", "c": "e"}
+         * beginningAndEnding(["man", "moon", "main"]) → {"m": "n"}
+         * beginningAndEnding(["muddy", "good", "moat", "good", "night"]) → {"g": "d", "m": "t", "n": "t"}
+         */
+        public Dictionary<string, string> BeginningAndEnding(string[] words)
+        {
+            return null;
+        }
+    }
+}

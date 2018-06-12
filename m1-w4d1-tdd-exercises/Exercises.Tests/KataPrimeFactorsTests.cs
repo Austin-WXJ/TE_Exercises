3c1ce5cdb82a08e842e886a6ff70153420ff6049
unknown <austin.wxj@gmail.com>
2018-06-12 09:44:08 -0400

    making no progess

--- m1-w4d1-tdd-exercises/Exercises.Tests/KataPrimeFactorsTests.cs
+++ m1-w4d1-tdd-exercises/Exercises.Tests/KataPrimeFactorsTests.cs
@@ -7,6 +7,27 @@ namespace Exercises.Tests
     [TestClass]
     public class KataPrimeFactorsTests
     {
-        
+        KataPrimeFactors newFactor;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            newFactor = new KataPrimeFactors();
+        }
+        [TestMethod]
+        public void PrimeFactor_Test()
+        {
+            CollectionAssert.AreEqual(new int[] { 2 }, newFactor.Factor(2));
+            CollectionAssert.AreEqual(new int[] { 3 }, newFactor.Factor(3));
+            CollectionAssert.AreEqual(new int[] { 2, 2 }, newFactor.Factor(4));
+            CollectionAssert.AreEqual(new int[] { 2, 3 }, newFactor.Factor(6));
+            CollectionAssert.AreEqual(new int[] { 7 }, newFactor.Factor(7));
+            CollectionAssert.AreEqual(new int[] { 2, 2, 2 }, newFactor.Factor(8));
+            CollectionAssert.AreEqual(new int[] { 3, 3 }, newFactor.Factor(9));
+            CollectionAssert.AreEqual(new int[] { 2, 5 }, newFactor.Factor(10));
+
+            
+        }
+
     }
 }
--- m1-w4d1-tdd-exercises/Exercises/KataPrimeFactors.cs
+++ m1-w4d1-tdd-exercises/Exercises/KataPrimeFactors.cs
@@ -9,5 +9,25 @@ namespace Exercises
     /* OPTIONAL */
     public class KataPrimeFactors
     {
+        public int[] Factor(int n)
+        {
+            int F = 2;
+
+            List<int> result = new List<int>();
+
+            if (n % F != 0)
+            {
+                F++;
+            }
+            while (n % F == 0)
+            {
+                result.Add(F);
+                n /= F;
+
+            }
+
+
+            return result.ToArray();
+        }
     }
 }

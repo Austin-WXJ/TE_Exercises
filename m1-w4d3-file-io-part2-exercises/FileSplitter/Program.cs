1596cafc1f7ea8cf3db46a7c897af9adf477f3c9
austin.WXJ <austin.WXJ@gmail.com>
2018-06-18 10:56:24 -0400

    redundant

--- m1-w4d3-file-io-part2-exercises/FileSplitter/Program.cs
+++ m1-w4d3-file-io-part2-exercises/FileSplitter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,39 @@ namespace FileSplitter
     {
         static void Main(string[] args)
         {
+            int fileWriteCount = 0;
+            bool fileRead = false;
+            while (!fileRead)
+            {
+                try
+                {
+                    Console.WriteLine("What is the name of the input file?(please provide full path): ");
+                    string fullPath = Console.ReadLine();
+                    Console.WriteLine();
+                    int totalLineCount = File.ReadLines(fullPath).Count();
+
+                    Console.WriteLine("How many lines of text (max) should there be in the split files? ");
+                    int maxFileWriteLength = int.Parse(Console.ReadLine());
+
+                    fileWriteCount = (int)Math.Ceiling((decimal)totalLineCount / maxFileWriteLength);
+
+                    Console.WriteLine($"The input file had " +
+                        $"{totalLineCount} lines of text, " +
+                        $"which will write to {fileWriteCount} files");
+                    fileRead = true;
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Error reading the file");
+                    Console.WriteLine(e.Message);
+                }
+            }
+            //writefile
+            for (int i = 0; i < fileW; i++)
+            {
+
+            }
         }
     }
 }

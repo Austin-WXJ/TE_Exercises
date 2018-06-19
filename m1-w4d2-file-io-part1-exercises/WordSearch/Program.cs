1ebeb8666e5aa11c1fe20bbe59752db356514a80
austin.WXJ <austin.WXJ@gmail.com>
2018-06-19 11:51:59 -0400

    repaired WordSearch Alice in Wonderland

--- m1-w4d2-file-io-part1-exercises/WordSearch/Program.cs
+++ m1-w4d2-file-io-part1-exercises/WordSearch/Program.cs
@@ -11,58 +11,74 @@ namespace WordSearch
     {
         static void Main(string[] args)
         {
-            Console.Write("What Word do you want to find in Alice in Wonderland? ");
-            string searchKey = Console.ReadLine();
-            Console.WriteLine();
-
-            Console.Write("is this search case insensitive?(Y/N): ");
-            string caseSearch = Console.ReadLine();
-            Console.WriteLine();
-
-            bool caseInsensitive = (caseSearch.ToUpper() == "Y");
-        
-            //Console.Write("What is the directory of text file to search? ");
-            //string directory = Console.ReadLine();
-            //string directory = Environment.CurrentDirectory;
-            string directory = (@"C:\Users\awarner\austinwarner-c-exercises\m1-w4d2-file-io-part1-exercises");
-
-            //Console.Write("what is the file to be searched? ");
-            //string fileName = Console.ReadLine();
-            string fileName = ("alices_adventures_in_wonderland.txt");
-
-            string fullPath = Path.Combine(directory, fileName);
-
             try
             {
-                using (StreamReader sr = new StreamReader(fullPath))
+                bool done = false;
+
+                while (!done)
                 {
-                    int lineNumber = 0;
-                    int keywordCount = 0;
+                    Console.Write("What Word do you want to find in Alice in Wonderland? ");
+                    string searchKey = Console.ReadLine();
+                    Console.WriteLine();
+
+                    Console.Write("is this search case insensitive?(Y/N): ");
+                    string caseSearch = Console.ReadLine();
+                    Console.WriteLine();
+
+                    bool caseInsensitive = (caseSearch.ToUpper() == "Y");
 
 
-                    while (!sr.EndOfStream)
+                    string directory = (@"C:\Users\awarner\austinwarner-c-exercises\m1-w4d2-file-io-part1-exercises");
+
+                    string fileName = ("alices_adventures_in_wonderland.txt");
+
+                    string fullPath = Path.Combine(directory, fileName);
+
+                    using (StreamReader sr = new StreamReader(fullPath))
                     {
-                        string line = sr.ReadLine();
-                        string line2 = "";
-                        string searchKey2 = "";
+                        int lineNumber = 0;
+                        int keywordCount = 0;
 
-                        if (caseInsensitive)
+                        while (!sr.EndOfStream)
                         {
-                            line2 = line.ToUpper();
-                            searchKey2 = searchKey.ToUpper();
+                            string line = sr.ReadLine();
+                            string line2 = "";
+                            string searchKey2 = "";
+
+                            if (caseInsensitive)
+                            {
+                                line2 = line.ToUpper();
+                                searchKey2 = searchKey.ToUpper();
+                            }
+                            lineNumber++;
+
+                            if (line.Contains(searchKey))
+                            {
+                                Console.WriteLine($"({lineNumber}) {line}");
+                                keywordCount++;
+                            }
+                            else if (caseInsensitive && line2.Contains(searchKey2))
+                            {
+                                Console.WriteLine($"({lineNumber}) {line}");
+                                keywordCount++;
+                            }
                         }
-
-                        lineNumber++;
-
-                        if (line.Contains(searchKey) || line2.Contains(searchKey2) )
-                        {
-                            Console.WriteLine($"({lineNumber}) {line}");
-                            keywordCount++;
-                        }
-
+                        Console.WriteLine();
+                        Console.WriteLine($"\"{searchKey}\" was found {keywordCount} times.");
                     }
                     Console.WriteLine();
-                    Console.WriteLine($"\"{searchKey}\" was found {keywordCount} times.");
+                    Console.Write("Perform another search?(Y/N): ");
+                    string response = Console.ReadLine();
+
+                    switch (response.ToUpper())
+                    {
+                        case "Y":
+                            break;
+                        case "N":
+                            done = true;
+                            break;
+                    }
+                    Console.Clear();
                 }
             }
             catch (IOException e)
@@ -70,9 +86,7 @@ namespace WordSearch
                 Console.WriteLine("Error reading the file");
                 Console.WriteLine(e.Message);
             }
-            Console.WriteLine();
-            Console.ReadLine();
         }
-
     }
 }
+

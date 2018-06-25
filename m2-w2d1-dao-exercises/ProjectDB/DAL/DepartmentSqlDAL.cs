9252298d0dedbccad2432c303cad0e8a2119139a
John Fulton <john@TechElevator.com>
2018-06-25 10:35:18 -0400

    m2-w2d1-dao-exercises

---
+++ m2-w2d1-dao-exercises/ProjectDB/DAL/DepartmentSqlDAL.cs
@@ -0,0 +1,36 @@
+﻿using ProjectDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectDB.DAL
+{
+    public class DepartmentSqlDAL
+    {
+        private string connectionString;
+
+        // Single Parameter Constructor
+        public DepartmentSqlDAL(string dbConnectionString)
+        {
+            connectionString = dbConnectionString;
+        }
+
+        public List<Department> GetDepartments()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CreateDepartment(Department newDepartment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateDepartment(Department updatedDepartment)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ m2-w2d1-dao-exercises/ProjectDB/DAL/EmployeeSqlDAL.cs
@@ -0,0 +1,35 @@
+﻿using ProjectDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectDB.DAL
+{
+    public class EmployeeSqlDAL
+    {
+        private string connectionString;
+
+        // Single Parameter Constructor
+        public EmployeeSqlDAL(string dbConnectionString)
+        {
+            connectionString = dbConnectionString;
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> Search(string firstname, string lastname)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> GetEmployeesWithoutProjects()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

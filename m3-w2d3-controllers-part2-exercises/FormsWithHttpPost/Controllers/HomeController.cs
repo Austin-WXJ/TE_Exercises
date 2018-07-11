45c5db7f5ced5ea93db05c52668560ba0cda1c5d
John Fulton <john@TechElevator.com>
2018-07-10 22:05:32 -0400

    m3-w2d3-controllers-part2-exercises

---
+++ m3-w2d3-controllers-part2-exercises/FormsWithHttpPost/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using FormsWithHttpPost.DAL;
+using FormsWithHttpPost.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormsWithHttpPost.Controllers
+{
+    public class HomeController : Controller
+    {
+        IReviewDAL dal;
+
+        public HomeController()
+        {
+            string connectionstring = ConfigurationManager.ConnectionStrings["SquirrelReviews"].ConnectionString;
+            dal = new ReviewSqlDAL(connectionstring);
+        }
+
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }        
+    }
+}
---
+++ m3-w2d3-controllers-part2-exercises/FormsWithHttpPost/DAL/ReviewSqlDAL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FormsWithHttpPost.Models;
+using System.Data.SqlClient;
+
+namespace FormsWithHttpPost.DAL
+{
+    public class ReviewSqlDAL : IReviewDAL
+    {
+        private string conectionstring;
+
+        public ReviewSqlDAL(string conectionstring)
+        {
+            this.conectionstring = conectionstring;
+        }
+
+
+        public List<Review> GetAllReviews()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveReview(Review newReview)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

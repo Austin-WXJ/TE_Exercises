5f34c31bb2670cf4390011fba2a9a9715f7beff8
austin.WXJ <austin.WXJ@gmail.com>
2018-07-23 11:47:53 -0400

    code review

--- m4-w1d1-js-intro-to-js-exercises/exercises.js
+++ m4-w1d1-js-intro-to-js-exercises/exercises.js
@@ -1,1 +1,17 @@
-﻿
+﻿function sumDouble(x, y){
+    if (x===y){
+        return 2*(x+y);
+    }
+    else{
+        return x+y;
+    }
+}
+
+function hasTeen(x, y, z){
+    if((x || y || z) >= 13 || (x || y || z)<= 19 ){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
